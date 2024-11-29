76630923c4542e4b71558a9be6ae4e7a2bddac21
pkritt <krittapat37819@gmail.com>
2024-11-29 16:40:51 +0700

    Update lab6_2.cpp

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,41 +1,51 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
+double deg2rad(double a1){
+    return a1 * M_PI / 180.0;
+}
+
+double rad2deg(double a2){
+    return a2 * 180.0/M_PI;
+}
+
+double findXComponent(double l1, double l2, double a1, double a2){
+    return (l1*cos(a1))+(l2*cos(a2));
+}
+
+double findYComponent(double l1, double l2, double a1, double a2){
+    return (l1*sin(a1))+(l2*sin(a2));
+}
+
+double pythagoras(double xcomp, double ycomp){
+    return sqrt((xcomp*xcomp)+(ycomp*ycomp));
+}
+
+void showResult(double result_vec_length,double result_vec_direction){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    cout << "Length of the resultant vector = " << result_vec_length << endl;
+    cout << "Direction of the resultant vector (deg) = " << result_vec_direction << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+}
 
 int main(){
-	int N;
-	cout << "Enter the number of student: ";
-	cin >> N;
-	string name[N];
-    int age[N];
-	int i=0;
-	while (i<N){
-	   
-	    cout << "Name of student "<< i+1<<" = ";
-    	cin >> name[i];
-	    cout << "Age of student "<<i+1<<" = ";
-	    cin >> age[i];
-	      i++;
-	}
-	
-	int key;
-	cout << "--------------------------------------\n";
-	cout << "Enter an age to search: ";
-	cin >> key;
-	cout << "--------------------------------------\n";
-	
-	i=0;
-	while (i<N){
-	    if (age[i]!=key){
-            
-	        
-	    }
-	    else{cout << (name[i])<<"\n";}
-	   
-	    i++;
-	}
+    double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
+    cout << "Enter length of the first vector: ";
+    cin >> l1;
+    cout << "Enter direction of the first vector (deg): ";
+    cin >> a1;
+    cout << "Enter length of the second vector: ";
+    cin >> l2;
+    cout << "Enter direction of the second vector (deg): ";
+    cin >> a2;
+    
+    a1 = deg2rad(a1);
+    a2 = deg2rad(a2);
+    xcomp = findXComponent(l1,l2,a1,a2);
+    ycomp = findYComponent(l1,l2,a1,a2);
+    result_vec_length = pythagoras(xcomp,ycomp);
+    result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
 
-	
-	cout << "--------------------------------------\n";
-	
-	return 0;
+    showResult(result_vec_length,result_vec_direction);
 }
