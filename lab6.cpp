642cc38878a290ab16e527b9e7dcb66179ecf8b3
pkritt <krittapat37819@gmail.com>
2024-11-29 15:19:41 +0700

    test

---
+++ lab6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	int i = 1;
+	int a = 0;
+	while(i=1){
+    cout << "Enter an integer: ";
+	cin >> n[a];
+	a++;
+	if (n[a]!=){}
+	
+		else{i++}
+	}
+    cout << "#Even numbers = ";
+    cout << "#Odd numbers = ";
+    return 0;
+}
